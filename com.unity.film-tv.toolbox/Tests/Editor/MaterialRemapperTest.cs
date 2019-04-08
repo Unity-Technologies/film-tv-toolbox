4d28cd4f27874f26d0cad8271f19c00fced6b9d0
Marie Fetiveau <marief@unity3d.com>
2019-04-08 13:38:48 -0400

    Tests: initial commit (FTV-19)

---
+++ com.unity.film-tv.toolbox/Tests/Editor/MaterialRemapperTest.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+class EditorExampleTest {
+
+    [Test]
+    public void EditorSampleTestSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+}
