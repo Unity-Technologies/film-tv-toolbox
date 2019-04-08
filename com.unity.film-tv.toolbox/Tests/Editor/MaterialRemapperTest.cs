df3dcb7e10bbcbc5b628b9a5021946b4a555de72
Marie Fetiveau <marief@unity3d.com>
2019-04-08 16:14:36 -0400

    Tests: add a proper test for MaterialRemapper (FTV-19)

--- com.unity.film-tv.toolbox/Tests/Editor/MaterialRemapperTest.cs
+++ com.unity.film-tv.toolbox/Tests/Editor/MaterialRemapperTest.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
-using UnityEditor;
-using UnityEngine.TestTools;
 using NUnit.Framework;
-using System.Collections;
+using System.Linq;
+using UnityEditor.Formats.Alembic;
+using UnityEditor.SceneManagement;
 
-class EditorExampleTest {
+class EditorExampleTest
+{
+    MaterialRemapperModel m_Model;
 
     [Test]
-    public void EditorSampleTestSimplePasses() {
-        // Use the Assert class to test conditions.
+    public void TestMeshListCount() {
+        Assert.That(m_Model.GetMeshCount() == 3);
+    }
+
+    [Test]
+    public void TestUniqueMeshListCount() {
+        Assert.That(m_Model.GetUniqueMeshList().Count == 2);
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+        // Create two GOs from the same source mesh
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // Create a GO from another mesh
+        GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        m_Model = ScriptableObject.CreateInstance<MaterialRemapperModel>();
+        m_Model.hideFlags = HideFlags.DontSave;
+
+        var transforms = GameObject.FindObjectsOfType<MeshRenderer>().Select(X => X.transform);
+        m_Model.FindMeshesAndMaterials(transforms.ToList());
     }
 }
