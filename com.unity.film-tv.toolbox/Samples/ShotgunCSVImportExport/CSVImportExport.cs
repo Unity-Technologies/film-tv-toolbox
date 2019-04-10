af2a74866c6493979a3c378b797388145db1863e
Marie Fetiveau <marief@unity3d.com>
2019-04-10 08:54:57 -0400

    ShotgunCSVImportExport: linting pass (FTV-19)

--- com.unity.film-tv.toolbox/Samples/ShotgunCSVImportExport/CSVImportExport.cs
+++ com.unity.film-tv.toolbox/Samples/ShotgunCSVImportExport/CSVImportExport.cs
@@ -4,57 +4,54 @@ using System.Linq;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Timeline;
-using UnityEngine.Playables;
 using UnityEditor;
 using UnityEditor.FilmTV.Toolbox;
 
 public static class CsvImportExport
 {
     const int k_FPS = 24;
-    const string k_importFileName = "shot.csv";
-    const string k_exportFileName = "export_from_unity.csv";
-    const string k_timelineAssetName = "shotgun_imported_timeline.asset";
-    static string k_pathToFile = PackageUtils.GetCallerRelativeToProjectFolderPath();
-    const string k_timelineObjectName = "Sample Shotgun CSV timeline";
+    const string k_ImportFileName = "shot.csv";
+    const string k_ExportFileName = "export_from_unity.csv";
+    const string k_TimelineAssetName = "shotgun_imported_timeline.asset";
+    static string s_PathToFile = PackageUtils.GetCallerRelativeToProjectFolderPath();
+    const string k_TimelineObjectName = "Sample Shotgun CSV timeline";
 
-    [UnityEditor.MenuItem("Window/Film-TV toolbox/Samples/Shotgun/Import CSV")]
+    [MenuItem("Window/Film-TV toolbox/Samples/Shotgun/Import CSV")]
     public static void ImportCsv ()
     {
-        TimelineAsset timeline;
-
-        timeline = ScriptableObject.CreateInstance("TimelineAsset") as TimelineAsset;
-        timeline.name = k_timelineObjectName;
+        var timeline = ScriptableObject.CreateInstance("TimelineAsset") as TimelineAsset;
+        timeline.name = k_TimelineObjectName;
         timeline.editorSettings.fps = k_FPS;
-        ControlTrack track = timeline.CreateTrack<ControlTrack>(null, "");
+        var track = timeline.CreateTrack<ControlTrack>(null, "");
 
-        foreach (var row in ReadCsvFile(Path.Combine(k_pathToFile, k_importFileName)))
+        foreach (var row in ReadCsvFile(Path.Combine(s_PathToFile, k_ImportFileName)))
         {
-            TimelineClip clip = track.CreateClip<ControlPlayableAsset>();
-            ControlPlayableAsset clipAsset  = clip.asset as ControlPlayableAsset;
+            var clip = track.CreateClip<ControlPlayableAsset>();
+            var clipAsset  = clip.asset as ControlPlayableAsset;
 
             clipAsset.name = row["Shot Code"];
             clip.displayName = row["Shot Code"];
             clip.start =  (Convert.ToDouble(row["Cut In"]) / k_FPS);
             // In Unity, the last frame is exclusive. frames goes : [start; end[
-            clip.duration = ((Convert.ToDouble(row["Cut Out"]) - Convert.ToDouble(row["Cut In"]) + 1 ) / k_FPS);
+            clip.duration = (Convert.ToDouble(row["Cut Out"]) - Convert.ToDouble(row["Cut In"]) + 1 ) / k_FPS;
         }
-        AssetDatabase.CreateAsset(timeline, Path.Combine(k_pathToFile, k_timelineAssetName ));
 
         Debug.Log($"Loaded {k_ImportFileName} from {s_PathToFile}.");
+        AssetDatabase.CreateAsset(timeline, Path.Combine(s_PathToFile, k_TimelineAssetName ));
         Debug.Log($"Created Timeline Asset in {s_PathToFile}.");
     }
 
-    [UnityEditor.MenuItem("Window/Film-TV toolbox/Samples/Shotgun/Export CSV")]
+    [MenuItem("Window/Film-TV toolbox/Samples/Shotgun/Export CSV")]
     public static void ExportCsv()
     {
         // Get the actual timeline object
-        var timelineAsset = AssetDatabase.LoadAssetAtPath<TimelineAsset>(Path.Combine(k_pathToFile, k_timelineAssetName));
+        var timelineAsset = AssetDatabase.LoadAssetAtPath<TimelineAsset>(Path.Combine(s_PathToFile, k_TimelineAssetName));
 
         // Each dictionary in the list represents a row in the CSV file
         // Each value in the row is addressable by its column name
         var toWrite = new List<Dictionary<string, string>>();
 
-        TrackAsset track = timelineAsset.GetOutputTrack(0);
+        var track = timelineAsset.GetOutputTrack(0);
         foreach (var clip in track.GetClips())
         {
             var start = Convert.ToInt64(clip.start * k_FPS);
@@ -62,34 +59,38 @@ public static class CsvImportExport
             // remove the "padding" we added
             var end = Convert.ToInt64((clip.duration + clip.start) * k_FPS) - 1;
 
-            ControlPlayableAsset clipAsset  = clip.asset as ControlPlayableAsset;
+            var clipAsset  = clip.asset as ControlPlayableAsset;
             var shotCode = clipAsset.name;
 
-            var dict = new Dictionary<string, string>()
+            var dict = new Dictionary<string, string>
             {
                 {"Shot Code", shotCode },
                 {"Cut In", start.ToString() },
-                {"Cut Out", end.ToString() },
+                {"Cut Out", end.ToString() }
             };
             toWrite.Add(dict);
         }
 
-        var pathToOutputFile = Path.Combine(k_pathToFile, k_exportFileName);
+        var pathToOutputFile = Path.Combine(s_PathToFile, k_ExportFileName);
         Debug.Log($"Exported clip updates in {pathToOutputFile}");
         WriteCsv(toWrite, pathToOutputFile);
     }
 
-    private static List<Dictionary<string, string>> ReadCsvFile(string filePath)
+    static IEnumerable<Dictionary<string, string>> ReadCsvFile(string filePath)
     {
         var output = new List<Dictionary<string, string>>();
 
         using (var reader = new StreamReader(filePath))
         {
-            var fieldNamesList = reader.ReadLine().Split(',');
+            var line = reader.ReadLine();
+            if (line == null)
+                return output;
+
+            var fieldNamesList = line.Split(',');
             // Trim the double quotes
             fieldNamesList = fieldNamesList.Select(x => x.Replace("\"", "")).ToArray();
 
-            string line = reader.ReadLine();
+            line = reader.ReadLine();
 
             while (null != line)
             {
@@ -99,7 +100,7 @@ public static class CsvImportExport
                 // Trim the double quotes
                 values = values.Select(x => x.Replace("\"", "")).ToArray();
 
-                for (Int64 idx = 0; idx < fieldNamesList.Length; idx++)
+                for (long idx = 0; idx < fieldNamesList.Length; idx++)
                 {
                     dict.Add(fieldNamesList[idx], values[idx]);
                 }
@@ -112,16 +113,16 @@ public static class CsvImportExport
         return output;
     }
 
-    private static void WriteCsv (List<Dictionary<string, string>> input, string filePath)
+    static void WriteCsv (IReadOnlyList<Dictionary<string, string>> input, string filePath)
     {
         //Keys should be uniform
         var keys = input[0].Keys;
         using (var writer = new StreamWriter(filePath))
         {
-            writer.WriteLine(String.Join("\t", keys));
+            writer.WriteLine(string.Join("\t", keys));
             foreach (var item in input)
             {
-                writer.WriteLine(String.Join("\t", item.Values));
+                writer.WriteLine(string.Join("\t", item.Values));
             }
         }
     }
