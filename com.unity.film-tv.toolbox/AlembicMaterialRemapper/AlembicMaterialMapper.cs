05bda6eb7e7806f68992ed8da2860fb4a3d7a55f
Mike Wuetherick <mikew@unity3d.com>
2019-05-07 08:29:30 -0700

    new Alembic Material Mapper component

---
+++ com.unity.film-tv.toolbox/AlembicMaterialRemapper/AlembicMaterialMapper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Unity.FilmTV.Toolbox
+{
+    public class AlembicMaterialMapper : MonoBehaviour
+    {
+        public GameObject fbxLookDev;           // our master fbx that we look to for our source materials
+      
+        private List<Renderer> sourceMeshList = new List<Renderer>();
+        private List<Renderer> destMeshList = new List<Renderer>();
+
+        [ContextMenu("Sync Materials")]
+        public void SyncMaterials()
+        {
+            if (!fbxLookDev)
+            {
+                Debug.Log("AlembicMaterialMapper - connect source fbx to your alembic to automap materials");
+                return;
+            }
+
+            sourceMeshList = fbxLookDev.GetComponentsInChildren<Renderer>().ToList();
+            destMeshList = gameObject.GetComponentsInChildren<Renderer>().ToList();
+            
+            // sync them
+            foreach( var destMesh in destMeshList)
+            {
+                foreach( var sourceMesh in sourceMeshList )
+                {
+                    // alembic adds an empty parent node with the actual name we want, the mesh is contained underneath
+                    if (sourceMesh.name == destMesh.transform.parent.name)
+                    {
+                        destMesh.sharedMaterials = sourceMesh.sharedMaterials;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ com.unity.film-tv.toolbox/Editor/AlembicMaterialRemapper/AlembicMaterialRemapperInspector.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Unity.FilmTV.Toolbox
+{
+    [CustomEditor(typeof(AlembicMaterialMapper))]
+    public class AlembicMaterialRemapperInspector : Editor
+    {
+        public Object sourceObject;
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.Label("This component will remap materials from a source FBX to an Alembic, assuming the mesh & material structure are the same.", EditorStyles.helpBox);
+            GUILayout.Space(10f);
+           // DrawDefaultInspector();
+
+            var mapper = (AlembicMaterialMapper) target;
+
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label("Source Object: ");
+                sourceObject = EditorGUILayout.ObjectField(sourceObject, typeof(Object), true);
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10f);
+
+            if( sourceObject != null)
+            {
+                GUI.backgroundColor = new Color(0f, 64f, 0f);
+
+                if (GUILayout.Button("Sync Materials", GUILayout.Height(35f)))
+                {
+                    Undo.RegisterCompleteObjectUndo(target, "AlembicMaterialMapper");
+                    mapper.fbxLookDev = sourceObject as GameObject;
+                    mapper.SyncMaterials();
+                }
+            }
+            else
+            {
+                GUILayout.Label("Point the Source Object to an asset that contains the mesh / material structure that you wish to remap", EditorStyles.helpBox);
+            }
+            
+
+        }
+    }
+}
