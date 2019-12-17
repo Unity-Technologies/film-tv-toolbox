46eb8f56d752bd46ad90c40c7c7634a459752e8b
Mike Wuetherick <mikew@unity3d.com>
2019-12-17 14:35:48 -0800

    Multi-scene loading system - edit mode & runtime mode

---
+++ com.unity.film-tv.toolbox/Editor/MultiScene/MultiSceneLoaderEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Unity.FilmTV.Toolbox.MultiScene
+{
+    /// <summary>
+    /// Custom inspector for the MultiScene Scriptable Object
+    /// </summary>
+    [CustomEditor(typeof(MultiSceneLoader))]
+	public class MultiSceneLoaderEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var sceneConfig = (MultiSceneLoader)target;
+
+            EditorGUILayout.Space();
+            GUILayout.BeginVertical();
+            {
+                GUILayout.Label("Multi-Scene Config", EditorStyles.boldLabel);
+
+                GUILayout.Label("Allows you to define sets of scenes that can be loaded either as one 'set' or individually as desired. Useful for defining subsets of a project that different team members can work on independently.", EditorStyles.wordWrappedLabel);
+
+                DrawDefaultInspector(); // TODO: make a proper inspector for these so I don't need to use the default inspector
+
+                EditorGUILayout.Space();
+                GUILayout.Label("Load All Scenes", EditorStyles.boldLabel);
+                EditorGUILayout.Space();
+                if (GUILayout.Button("Load All Scenes", GUILayout.MinHeight(100), GUILayout.Height(50)))
+                    sceneConfig.LoadAllScenes();
+                GUILayout.Label("Loads all Main Scenes and then all Set Scenes", EditorStyles.wordWrappedLabel);
+
+                EditorGUILayout.Space();
+                GUILayout.Label("Load Sub Config Scenes", EditorStyles.boldLabel);
+                GUILayout.Label("Load scenes defined in a specific config, from above.", EditorStyles.wordWrappedLabel);
+
+                foreach (var entry in sceneConfig.config)
+                {
+                    EditorGUILayout.Space();
+                    if (GUILayout.Button("Load " + entry.name + " Scenes", GUILayout.MinHeight(100), GUILayout.Height(50)))
+                        sceneConfig.LoadSceneConfig(entry);
+                    GUILayout.Label("Loads ONLY the scenes defined in " + entry.name + ".", EditorStyles.wordWrappedLabel);
+                }
+            }
+            GUILayout.EndVertical();
+		}
+	}
+}
---
+++ com.unity.film-tv.toolbox/Samples/MultiScene/RuntimeLoadSample/TestRuntimeLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.FilmTV.Toolbox.MultiScene.Samples
+{
+    public class TestRuntimeLoader : MonoBehaviour
+    {
+        public MultiSceneLoader sceneConfig;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+            Debug.Log("NOTE: Must add all scenes from package samples into project build settings to test runtime loading");
+
+            if ( sceneConfig != null)
+            {
+                StartCoroutine(LoadConfig());
+            }
+        }
+
+        private IEnumerator LoadConfig()
+        {
+            Debug.Log("Testing runtime scene config loading....");
+            yield return new WaitForSeconds(2);
+
+            Debug.Log("Loading ALL scenes from config");
+            yield return new WaitForSeconds(1);
+            sceneConfig.LoadAllScenes();
+            yield return new WaitForSeconds(1);
+
+            Debug.Log("Load ALL scenes - COMPLETE");
+            yield return new WaitForSeconds(2);
+            
+            Debug.Log("Loading individual scene configs");
+
+            yield return new WaitForSeconds(1);
+            foreach( var thisConfig in sceneConfig.config)
+            {
+                Debug.Log("Loading scenes from config : " + thisConfig.name);
+                yield return new WaitForSeconds(1);
+                sceneConfig.LoadSceneConfig(thisConfig);
+                Debug.Log("Load  scenes from config : " + thisConfig.name + " - COMPLETE");
+                yield return new WaitForSeconds(2);
+            }
+
+            Debug.Log("Load individual scene config - COMPLETE");
+            yield return new WaitForSeconds(1);
+            Debug.Log("Test Runtime Scene loading - COMPLETE");
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
